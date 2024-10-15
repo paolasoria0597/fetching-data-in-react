d41896db450aa225e70f597c84b35b535886fde3
paolasoria <paola.soria0597@gmail.com>
2024-10-15 15:01:19 -0400

    create button for fecthing weather in new york

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import * as weatherService from './services/weatherService';
 // src/App.jsx
 
 const App = () => {
-
+const fetchData= async () => {
+  const data= await weatherService.show('New York');
+  console.log('Data:', data);
+};
   return (
-    <h1>Hello world!</h1>
+    <main>
+      <h1>Weather API</h1>
+      <button onClick={fetchData}> Fetch Weather Data </button>
+    </main>
   );
 }
 
