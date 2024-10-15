42952a94640413237f9dd65d74a3ca3908d846cc
paolasoria <paola.soria0597@gmail.com>
2024-10-15 16:04:56 -0400

    add default weather fetch to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import * as weatherService from './services/weatherService';
 import weatherSearch from './components/WeatherSearch.jsx';
@@ -6,6 +6,19 @@ import WeatherDetails from './components/WeatherDetails.jsx';
 
 const App = () => {
   const [weather, setWeather]= useState({});
+useEffect(()=> {
+  const fetchDefaultData = async () => {
+    const data = await weatherService.show('New York');
+    const newWeatherState = {
+      location: data.location.name,
+      temperature: data.current.temp_f,
+      condition: data.current.condition.text,
+    };
+    setWeather(newWeatherState);
+  };
+   fetchDefaultData();
+}, []);
+
 
 const fetchData= async (city) => {
   const data= await weatherService.show(city);
